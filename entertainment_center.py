b3296fe8e4d7210acabe3fd0ab577e316cf49acd
Jeff Berlin <jeff@jbrally.com>
2017-06-24 21:15:58 -0400

    Fixed URL issue

--- entertainment_center.py
+++ entertainment_center.py
@@ -17,7 +17,8 @@ dumb_and_dumber = media.Movie(
     "Dumb and Dumber",
     "Two friends who try to return someone's property, "
     "but find themselves in a sticky situation",
-    "http://t0.gstatic.com/images?q=tbn:ANd9GcRfKxcLRaftGDJ-q6WataYprMWOROBAhNPxuqjCUL8vaCA6ZaW1",  # noqa
+    "http://t0.gstatic.com/images?q=tbn:"
+    "ANd9GcRfKxcLRaftGDJ-q6WataYprMWOROBAhNPxuqjCUL8vaCA6ZaW1",  # noqa
     "https://www.youtube.com/watch?v=l13yPhimE3o")
 # print(dumb_and_dumber.storyline)
 # dumb_and_dumber.show_trailer()
@@ -27,28 +28,32 @@ black_hawk_down = media.Movie(
     "Based on the true story of a war waged in Mogadishu "
     "by the high tech Black Hawk helicopters "
     "and the US Army Rangers and Deltas",
-    "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_hawk_down_ver1.jpg",  # noqa
+    "https://upload.wikimedia.org/wikipedia/"
+    "en/0/0c/Black_hawk_down_ver1.jpg",  # noqa
     "https://www.youtube.com/watch?v=tnV6wM-vd9s")
 
 hangover = media.Movie(
     "The Hangover",
     "Three buddies wake up from a bachelor party in Las Vegas, "
     "with no memory of the previous night and the bachelor is missing",
-    "https://upload.wikimedia.org/wikipedia/en/b/b9/Hangoverposter09.jpg",  # noqa
+    "https://upload.wikimedia.org/wikipedia/en/"
+    "b/b9/Hangoverposter09.jpg",  # noqa
     "https://www.youtube.com/watch?v=tcdUhdOlz9M")
 
 waterboy = media.Movie(
     "The Waterboy",
     "A waterboy for a college football team discovers he has a unique "
     "tackling ability and becomes a member of the team",
-    "http://games.lukky.us/wp-content/uploads/imdbcache/tt0120484.jpg",  # noqa
+    "http://games.lukky.us/wp-content/uploads/"
+    "imdbcache/tt0120484.jpg",  # noqa
     "https://www.youtube.com/watch?v=z8yv9eq5s14")
 
 step_brothers = media.Movie(
     "Step Brothers",
     "Two aimless middle-aged losers still living at home are forced "
     "against their will to become roommates when their parents marry",
-    "https://upload.wikimedia.org/wikipedia/en/d/d9/StepbrothersMP08.jpg",  # noqa
+    "https://upload.wikimedia.org/wikipedia/en/d/"
+    "d9/StepbrothersMP08.jpg",  # noqa
     "https://www.youtube.com/watch?v=CewglxElBK0")
 
 # Defines what is inside movies
